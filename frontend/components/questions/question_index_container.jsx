793de69636932e037b4522dac58f6d0be59c1dcc
Klhang <chrisjunhang@gmail.com>
2017-09-24 14:38:12 -0700

    set up questions component

---
+++ frontend/components/questions/question_index_container.jsx
@@ -0,0 +1,21 @@
+import { connect } from "react-redux";
+import {
+  fetchQuestions,
+  deleteQuestion,
+  createQuestion
+} from "../../actions/question_actions";
+import QuestionIndex from "./question_index";
+
+const mapStateToProps = state => {
+  return {
+    questions: Object.keys(state.questions).map(id => state.questions[id])
+  };
+};
+
+const mapDispatchToProps = dispatch => ({
+  fetchQuestions: () => dispatch(fetchQuestions()),
+  deleteQuestion: id => dispatch(deleteQuestion(id)),
+  createQuestion: question => dispatch(createQuestion(question))
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(QuestionIndex);
