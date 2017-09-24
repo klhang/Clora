7683cb2f1b26f648ec44369403a0c68dbebbefed
Klhang <chrisjunhang@gmail.com>
2017-09-24 10:55:52 -0700

    create quesiton controller

---
+++ app/controllers/api/questions_controller.rb
@@ -0,0 +1,35 @@
+class Api::QuestionsController < ApplicationController
+
+  def index
+    @questions = Question.all
+  end
+
+  def show
+    @question = Question.find(params[:id])
+  end
+
+  def create
+    @question = current_user.questions.create!(question_params)
+    @question.save
+    render :show
+  end
+
+  def update
+    @question = current_user.questions.find(params[:id])
+    @question.update_attributes(question_params)
+    render :show
+  end
+
+  def destroy
+    @question = current_user.questions.find(params[:id])
+    @question.destroy
+    redirect_to "/#/"
+  end
+
+  private
+
+  def question_params
+    params.require(:question).permit(:title, :description)
+  end
+
+end
