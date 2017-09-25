d95f7326200268cd958e770da950f3e03f74a90c
Klhang <chrisjunhang@gmail.com>
2017-09-25 15:04:44 -0700

    create answer actions

---
+++ frontend/actions/answer_actions.js
@@ -0,0 +1,41 @@
+import * as AnswerApiUtil from "../util/answer_api_util";
+
+export const RECEIVE_ALL_ANSWERS = "RECEIVE_ALL_ANSWERS";
+export const RECEIVE_ANSWER = "RECEIVE_ANSWER";
+export const REMOVE_ANSWER = "REMOVE_ANSWER";
+
+export const fetchAnswers = () => dispatch =>
+  AnswerApiUtil.fetchAnswers().then(answers =>
+    dispatch(receiveAllAnswers(answers))
+  );
+
+export const fetchAnswer = id => dispatch =>
+  AnswerApiUtil.fetchAnswer(id).then(answer => dispatch(receiveAnswer(answer)));
+
+export const createAnswer = answer => dispatch =>
+  AnswerApiUtil.createAnswer(answer).then(newAnswer =>
+    dispatch(receiveAnswer(newAnswer))
+  );
+
+export const updateAnswer = answer => dispatch =>
+  AnswerApiUtil.updateAnswer(answer).then(updatedAnswer =>
+    dispatch(receiveAnswer(updatedAnswer))
+  );
+
+export const deleteAnswer = id => dispatch =>
+  AnswerApiUtil.deleteAnswer(id).then(answer => dispatch(removeAnswer(answer)));
+
+const receiveAllAnswers = answers => ({
+  type: RECEIVE_ALL_ANSWERS,
+  answers
+});
+
+const receiveAnswer = answer => ({
+  type: RECEIVE_ANSWER,
+  answer
+});
+
+const removeAnswer = answer => ({
+  type: REMOVE_ANSWER,
+  answer
+});
