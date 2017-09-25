bf34b19d8d41d7b1c6d47fa552bf6677cc504f1c
Klhang <chrisjunhang@gmail.com>
2017-09-25 06:37:05 -0700

    update component root file

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -6,6 +6,9 @@ import GreetingContainer from "./greeting/greeting_container";
 import SessionFormContainer from "./session_form/session_form_container";
 import { AuthRoute, ProtectedRoute } from "../util/route_util";
 
+import QuestionIndexContainer from "./questions/question_index_container";
+import QuestionShowContainer from "./questions/question_show_container";
+
 const App = () => (
   <div>
     <header>
@@ -14,9 +17,13 @@ const App = () => (
       </Link>
       <GreetingContainer />
     </header>
+
     <Switch>
       <AuthRoute path="/login" component={SessionFormContainer} />
       <AuthRoute path="/signup" component={SessionFormContainer} />
+
+      <IndexRoute component={QuestionIndexContainer} />
+      <Route path="/questions/:questionId" component={QuestionShowContainer} />
     </Switch>
   </div>
 );
