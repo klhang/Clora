a70612b5418f5204395bf6f243e19aaffaf1626c
Klhang <chrisjunhang@gmail.com>
2017-09-25 15:19:57 -0700

    create answer reducer and modify root reducer

--- frontend/reducers/root_reducer.js
+++ frontend/reducers/root_reducer.js
@@ -2,6 +2,7 @@ import { combineReducers } from "redux";
 
 // import entities from "./entities_reducer";
 // import ui from "./ui_reducer";
+import answers from "./answers_reducer";
 import questions from "./questions_reducer";
 import session from "./session_reducer";
 import errors from "./errors_reducer";
@@ -10,6 +11,7 @@ const RootReducer = combineReducers({
   // entities,
   session,
   questions,
+  answers,
   // ui,
   errors
 });
