1ddd518511b85733c8052082758d44cd499a6910
Klhang <chrisjunhang@gmail.com>
2017-09-25 17:53:36 -0700

    update question_index page

--- frontend/components/questions/question_index_item.jsx
+++ frontend/components/questions/question_index_item.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link, hashHistory } from "react-router";
+import AnswerShowContainer from "../answers/answer_show_container";
 
 class QuestionIndexItem extends React.Component {
   constructor(props) {
@@ -7,14 +8,42 @@ class QuestionIndexItem extends React.Component {
     this.state = { index: null };
   }
 
+  componentDidMount() {
+    this.setAnswerIndex();
+  }
+
+  setAnswerIndex() {
+    if (this.state.index === null) {
+      let index1 = Math.floor(
+        Math.random() * this.props.question.answers.length
+      );
+      this.setState({ index: index1 });
+    }
+  }
+
   render() {
     const question = this.props.question;
+
+    let answer = question.answers[this.state.index];
+
+    if (!answer) {
+      return (
+        <li className="QuestionItem">
+          <Link to={`/questions/${question.id}`}>
+            <h2 className="QuestionItemQuestion">{question.question}</h2>
+          </Link>
+        </li>
+      );
+    }
 
     return (
       <li className="QuestionItem">
         <Link to={`/questions/${question.id}`}>
-          <h2 className="QuestionItemQuestion">{question.title}</h2>
+          <h2 className="QuestionItemQuestion">{question.question}</h2>
         </Link>
+        <ul>
+          <AnswerShowContainer answerId={answer.id} />
+        </ul>
       </li>
     );
   }
