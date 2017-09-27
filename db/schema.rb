3fad7d7b06ce6368d994b7199e9a3384e970a7cd
Klhang <chrisjunhang@gmail.com>
2017-09-27 14:59:01 -0700

    create topic table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170927073022) do
+ActiveRecord::Schema.define(version: 20170927213802) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,13 @@ ActiveRecord::Schema.define(version: 20170927073022) do
     t.datetime "updated_at", null: false
     t.index ["author_id"], name: "index_questions_on_author_id"
     t.index ["title"], name: "index_questions_on_title"
+  end
+
+  create_table "topics", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_topics_on_name", unique: true
   end
 
   create_table "users", force: :cascade do |t|
